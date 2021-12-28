ee49cdcddfcf55ea668bc628ae703cf1235e4c3b
JunHyuk Kim <kwk8002@naver.com>
2021-12-28 22:51:30 +0900

    algorithm-input_output

--- backJun_12_28.py
+++ backJun_12_28.py
@@ -14,3 +14,16 @@
 
 #     except :
 #         break
+
+#백준 10952번 문제 
+
+while True :
+    a,b = map(int, input().split())
+    if a != 0 and b != 0 : 
+        print(a+b)
+    else :
+        break
+   
+
+    
+
