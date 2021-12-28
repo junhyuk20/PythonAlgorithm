55002479d72a3e1be3d9e434bcfc764549989b96
JunHyuk Kim <kwk8002@naver.com>
2021-12-28 22:41:01 +0900

    third commit

--- backJun_12_28.py
+++ backJun_12_28.py
@@ -1,6 +1,16 @@
 # 백준 10950번 문제
-n = int(input())
+# n = int(input())
 
-for i in range(1,n+1) :
-    a,b = map(int, input().split())
-    print(a+b)
+# for i in range(1,n+1) :
+#     a,b = map(int, input().split())
+#     print(a+b)
+
+
+#백준 10951번 문제
+# while True :
+#     try :
+#         a,b = map(int, input().split())
+#         print(a+b)
+
+#     except :
+#         break
