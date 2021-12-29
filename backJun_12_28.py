db3d1c03ec0a0ebeae59bb03377caa4ac720c380
JunHyuk Kim <kwk8002@naver.com>
2021-12-29 23:42:48 +0900

    second inputOutPut

--- backJun_12_28.py
+++ backJun_12_28.py
@@ -16,14 +16,55 @@
 #         break
 
 #백준 10952번 문제 
+# while True :
+#     a,b = map(int, input().split())
+#     if a != 0 and b != 0 : 
+#         print(a+b)
+#     else :
+#         break
 
-while True :
-    a,b = map(int, input().split())
-    if a != 0 and b != 0 : 
-        print(a+b)
-    else :
-        break
+#백준 10953번 문제
+# n = int(input())
+
+# for i in range(1,n+1):
+#     a,b = map(int, input().split(","))
+#     print(a+b)
    
+# 백준 11201번 문제
+# n = int(input())
 
+# for i in range(1,n+1) :
+#     a,b = map(int, input().split())
+#     print("Case #{}: {}".format(i,a+b))
+
+# 백준 11022번 문제
+# n = int(input())
+
+# for i in range(1,n+1):
+#     a,b = map(int,input().split())
+#     print("Case #{}: {} + {} = {}".format(i,a,b,a+b))
+
+#백준 11718번 문제
+# while True:
+#     try:
+#         print(input())
+#     except :
+#         break
+
+#백준 11719번 문제
+# while True:
+#     try:
+#         print(input())
+#     except :
+#         break
     
+#백준 11720번 문제
+# n = input()
+# print(sum(map(int, input())))
 
+# n = input()
+# nums = input()
+# total = 0
+# for i in nums :
+#     total += int(i)  # total= total+int(i)
+# print(total)
---
+++ backJun_12_29.py
@@ -0,0 +1,50 @@
+#백준 2240번 문제
+# n = int(input())
+
+# for i in range(1,n+1):
+#     print("*" * ((n+1)-i))
+
+
+#백준 2241번 문제
+# n = int(input())
+
+# for i in range(1,n+1):
+#     print((" " * (i-1)) + "*" * ((n+1)-i) )
+
+#백준 2442번 문제
+# n = int(input())
+
+# for i in range(1,n+1):
+#     print(" " * (n-i) + "*" * ((2*i)-1))
+
+
+#백준 2445번 문제 첫번째 풀이 방식 틀렸다고 나옴...
+# n = int(input())
+
+# for i in range(1,n+1):
+#     print("*" * i +  " " * (n-i) + " " * (n-i) + "*" * i)
+#     if i == 5 :
+#         for j in range(1,n+1):
+#             print("*" * (n-j) + " " * j + " " * j + "*" * (n-j) )
+
+#백준 2445번 문제 두번째 풀이 방식
+# n = int(input())
+
+# for i in range(1,n+1):
+#     print("*" * i + " " *((n*2)- (i*2)) + "*" * i)
+# for i in range(n-1,0,-1) :
+#     print("*" * i + " " *((n*2)- (i*2)) + "*" * i)
+
+#백준 2522번 문제
+# n = int(input())
+
+# for i in range(1,n+1):
+#     print(" " * (n-i) + "*" *i)
+
+# for i in range(n-1,0,-1):
+#     print(" " * (n-i) + "*" *i)
+
+
+
+
+
