afdd5a0b57dddc68e64a91922556c768f6e1b6db
JunHyuk Kim <kwk8002@naver.com>
2022-01-15 16:56:08 +0900

    heapq,queue문제

--- 자료구조/Priority_Queue_Heapq.py
+++ 자료구조/Priority_Queue_Heapq.py
@@ -6,21 +6,22 @@
 # 또한 다른 언어에서 pop()함수를 통해서 빼기만 하지만 파이썬은 get()함수를 사용하며 빠지는 놈이 누구인지도 출력후 빠진다
 
 # PriorityQueue의 느린속도의 대응책 heapq(=힙큐)
-import heapq # 여기서 heapq로 쓰기 귀찮다 하면 ✨as hq로 줄여서 사용할 수 있다.
+import heapq as hq # 여기서 heapq로 쓰기 귀찮다 하면 ✨as hq로 줄여서 사용할 수 있다.
+import sys
 
 #✨사용법
 
-pq = []                 # 1. 배열선언을 해준다
-heapq.heappush(pq, 6)   # 2. heap.heappush를 통해 수를 넣어준다. 넣어줄때 선언한 배열을 쓰고, 원하는 수를 넣는다.
-heapq.heappush(pq, 20)
-heapq.heappush(pq, 80)
-heapq.heappush(pq, -120)
-heapq.heappush(pq, -70)
+# pq = []                 # 1. 배열선언을 해준다
+# heapq.heappush(pq, 6)   # 2. heap.heappush를 통해 수를 넣어준다. 넣어줄때 선언한 배열을 쓰고, 원하는 수를 넣는다.
+# heapq.heappush(pq, 20)
+# heapq.heappush(pq, 80)
+# heapq.heappush(pq, -120)
+# heapq.heappush(pq, -70)
 
-print(pq)               # 위에서 pq가 배열로 선어 되어있기 때문에 순서대로 들어가지 않을까 라고 생각하지만 heapq.heappush() 통해서 들어 가면 알아서 mini-heap순서로 바꿔준다
-while pq:
-    print(pq[0])        # 0번쨰는 최상단이므로 mini-heap인 제일 작은수가 오게 된다.
-    heapq.heappop(pq)
+# print(pq)               # 위에서 pq가 배열로 선어 되어있기 때문에 순서대로 들어가지 않을까 라고 생각하지만 heapq.heappush() 통해서 들어 가면 알아서 mini-heap순서로 바꿔준다
+# while pq:
+#     print(pq[0])        # 0번쨰는 최상단이므로 mini-heap인 제일 작은수가 오게 된다.
+#     heapq.heappop(pq)
 
 
 #이방법 또한 안전한 스레드를 사용해서 하므로 연산 시간이 조금더 오래 걸리므로 코테에서 사용하지 않음
@@ -36,3 +37,18 @@ while pq:
 # while not pq.empty():
 #     print(pq)
 #     print(pq.get) # 파이썬에서는 get이 pop과 같다 
+
+
+# 백준 11286번 문제 절대값 힙 !! 
+# 이문제에서는 n값이 최대 10만이므로 input()을 사용하면 너무 느리기 때문에 sys.readLine() 사용해서 푼다
+# 변수안에 리드라인을 넣고 사용할때에는 readline ✨() 괄호를 넣으면 안된다. input에 넣고 사용하는건 그냥 사용자 스타일임
+# abs()함수가 절대값을 찾는 함수이다 --> 음수를 넣으면 양수가 나오고 양수를 넣어도 양수가 나온다.
+input = sys.stdin.readline 
+test = []
+for i in range (int(input())):
+    x = int(input())
+    if x != 0 :
+        hq.heappush(test, (abs(x),x)) 
+    else :
+        print(hq.heappop(test)[1] if test else 0)
+         
--- 자료구조/Queue.py
+++ 자료구조/Queue.py
@@ -5,13 +5,41 @@
 # 위와 같은 이유로 파이썬에서 큐 자료구조를 사용해야 될때에는 deque를 사용하면 된다.
 from collections import deque # deque는 앞에서 추가 앞에서 삭제, 뒤에서 추가 뒤에서 삭제 양방향 가능한 자료구조
 
-dq = deque()
-dq.append(123) # 뒤에서부터 삽입
-dq.appendleft(456) # 앞에서부터 삽입
-dq.appendleft(789)
+# dq = deque()
+# dq.append(123) # 뒤에서부터 삽입
+# dq.appendleft(456) # 앞에서부터 삽입
+# dq.appendleft(789)
 
-print(dq)
+# print(dq)
 
-print(dq.pop()) # 뒤에서부터 삭제
-print(dq.popleft()) # 앞에서부터 삭제
-print(dq)
+# print(dq.pop()) # 뒤에서부터 삭제
+# print(dq.popleft()) # 앞에서부터 삭제
+# print(dq)
+
+#✨ 이문제의 핵심은 2초안에 푸는 것이다. 그러므로 시간 복잡도를 이제 항상 생각하면서 풀어야 되는게 핵심인 문제이다. 
+# # 백준 2164번 카드2 문제  
+# N =int(input())
+# dq = deque() 
+# for i in range(1, N+1): 
+#     dq.append(i)
+
+# #print(dq)
+# while len(dq) > 1: # 마지막 하나만 찾아야 하니깐 길이가 1일때 멈춰라
+#     dq.popleft()   # 맨앞에 놈을 삭제 
+#     dq.append(dq.popleft()) # 그다음 맨앞을 삭제하고 추가를해주면 문제에서 봤던 맨아래에 넣는 형식이 된다. 굿
+
+# print(dq.popleft()) 
+
+#✨ 이문제를 배열로 풀게 된다면 시간복잡도는 O(N^2) 이 되므로 최대 50만인 수가 왔을 떄는 벌써 2초를 넘겨 버린다 그러므로 사용할 수없는 방법이다.
+# 백준 2164번 카드2 문제  배열로 풀기 
+N =int(input())
+arr = []
+for i in range(1, N+1) :
+    arr.append(i)
+    
+#print(dq)
+while len(arr) > 1: # 마지막 하나만 찾아야 하니깐 길이가 1일때 멈춰라
+    arr.pop(0)   # 맨앞에 놈을 삭제 
+    arr.append(arr.pop(0)) # 그다음 맨앞을 삭제하고 추가를해주면 문제에서 봤던 맨아래에 넣는 형식이 된다. 굿
+
+print(arr.pop()) 
